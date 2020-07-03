f57566dd505b58a365bf453daaaabecf0125b029
huhao <415328529@qq.com>
2020-07-03 18:02:03 +0800

    优化脚本

--- k8x.py
+++ k8x.py
@@ -14,7 +14,7 @@ def delete_label_pod(label_selector, delay=120):
     pods = api.list_pod_for_all_namespaces(
         watch=False, label_selector=label_selector)
     for pod in pods.items:
-        print(f"delete {pod.metadata.name}")
+        print(f"""{time.strftime("%Y-%m-%d %H:%M:%S")} delete {pod.metadata.name}""")
         api.delete_namespaced_pod(pod.metadata.name, pod.metadata.namespace)
         if delay > 0:
             time.sleep(delay)
@@ -23,24 +23,31 @@ def delete_label_pod(label_selector, delay=120):
 def delete_node_pod(node_name, namespace="*", delay=120):
     pods = list_pod_for_node(node_name)
     for pod in pods:
-        owner_kind = pod.metadata.owner_references[0].kind
-        if owner_kind == "DaemonSet":
+        if pod.metadata.owner_references is not None:
+            owner_kind = ",".join(
+                [o.kind for o in pod.metadata.owner_references])
+            if owner_kind == "DaemonSet":
+                print(
+                    f"""{time.strftime("%Y-%m-%d %H:%M:%S")} skip {pod.metadata.name}""")
+                continue
+        else:
+            print(
+                f"""{time.strftime("%Y-%m-%d %H:%M:%S")} skip {pod.metadata.name}""")
             continue
         if namespace == "*" or pod.metadata.namespace == namespace:
-            print(f"delete {pod.metadata.name}")
+            print(
+                f"""{time.strftime("%Y-%m-%d %H:%M:%S")} delete {pod.metadata.name}""")
             api.delete_namespaced_pod(
                 pod.metadata.name, pod.metadata.namespace)
             if delay > 0:
                 time.sleep(delay)
 
 
-# delete_node_pod("cn-hangzhou.172.18.193.232", "kube-system", 30)
-# delete_node_pod("cn-hangzhou.172.18.194.10", "kube-system", 30)
+# delete_node_pod("cn-hangzhou.172.18.194.23", "kube-system", 30)
 # delete_label_pod("app=mysql")
-delete_label_pod("app=postgres")
-delete_label_pod("app=mongo")
-delete_label_pod("app=redis")
-delete_label_pod("app=elasticsearch")
-delete_label_pod("app=kafka")
-delete_node_pod("cn-hangzhou.172.18.193.232")
-delete_node_pod("cn-hangzhou.172.18.194.10")
+# delete_label_pod("app=postgres")
+# delete_label_pod("app=mongo")
+# delete_label_pod("app=redis")
+# delete_label_pod("app=elasticsearch")
+# delete_label_pod("app=kafka")
+# delete_node_pod("cn-hangzhou.172.18.194.23", "*", 30)
